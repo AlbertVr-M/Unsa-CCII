c0b8d46e6bbdf7fb6cd3106237bbc92f4a2ad328
AlbertVr-M <avargasmaman@unsa.edu.pe>
2022-05-02 16:51:09 -0500

    Create Ejercicio1.cpp

---
+++ Laboratorio1/Ejercicio1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1, n2;
+
+    cout<<"Ingrese el primer numero: "; cin>>n1;
+
+    cout<<"Ingrese el segundo numero: "; cin>>n2;
+
+    cout<<"El producto de ambos numeros es: "<<n1*n2;
+
+
+    return 0;
+}
