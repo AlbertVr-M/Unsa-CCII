7dacef494274198d98aa39c7f6659f4e9775612c
AlbertVr-M <avargasmaman@unsa.edu.pe>
2022-05-03 10:56:47 -0500

    Ejercicios del Laboratorio 1

---
+++ Laboratorio1/Ejercicio5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float numero_decimal=0;
+    
+    cout<<"Ingrese un numero decimal: "; cin>>numero_decimal;
+
+    int numero_entero=(int)numero_decimal;
+    cout<<numero_entero;
+
+    return 0;
+
+}
