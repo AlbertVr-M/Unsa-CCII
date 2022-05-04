dd2b8a83bbc2b3e7e6be730d403e5a63287f682d
AlbertVr-M <avargasmaman@unsa.edu.pe>
2022-05-03 20:55:49 -0500

    Ejercicios del Laboratorio 3

--- Laboratorio3/Ejercicio1.cpp
+++ Laboratorio3/Ejercicio1.cpp
@@ -1,25 +1,29 @@
-#include<iostream>
+#include "iostream"
 using namespace std;
-int potencia(int base,int expo){
-	int pot;
-	if(expo==1){
-		pot = base;
-	}
-	else{
-		pot = base*potencia(base,expo-1);
-	}
-	return pot;
-}
-
 int main(){
-	int base,exponente;
-	cout<<"Escriba la numero base: ";
-	cin>>base;
-	cout<<"Escriba el exponente: ";
-	cin>>exponente;
-	cout<<"\nResultado: "<<potencia(base,exponente)<<endl;	
+    string nombre[5];
+    string apellidos[5];
+    string dni[5];
+    string edad[5];
+    cout << "Por favor ingrese la siguiente información de las personass: \n";
+    for(int i = 0; i < 3; i++){
+        cout << "\n******* Persona " << i + 1 <<"********:\n";
+        cout << "Nombre: ";
+        cin >> nombre[i];
+        cout << "apellidos: ";
+        cin>>apellidos[i];
+        //getline(cin,apellidos[i]); no me funciona con el getline
+        cout<<" DNI: ";
+        cin>>dni[i];
+        cout<<"Edad: ";
+        cin>>edad[i];
+    }
+    cout<<"\nLos datos de las Personas son: "<<endl;
+    for(int i=0;i<3;i++){ 
+        cout<<nombre[i]<<"-";
+        cout<<apellidos[i]<<"-";
+        cout<<dni[i]<<"-";
+        cout<<edad[i]<<endl;
+    }
 	return 0;
 }
-
-
-
